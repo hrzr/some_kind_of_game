8bf405b28647d2a5f32aecf2861f15448010dec9
Mikhail Gaivoronskii <kittengrinder@gmail.com>
2022-06-10 12:11:57 +0500

    added Gun object to main screen

--- space_invaders/space_invaders.py
+++ space_invaders/space_invaders.py
@@ -1,17 +1,23 @@
 import pygame
 import sys
+from gun import Gun
+
 
 def run():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Space invaders")
     bg_color = (0, 0, 0)
+    gun = Gun(screen)
 
     while True:
-        screen.fill(bg_color)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(bg_color)
+        gun.output()
         pygame.display.flip()
+
 
 run()
